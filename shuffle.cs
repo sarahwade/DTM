0b81f5bf46e95ee8840284bbc9cd57dc66e9a542
sarahwade <sarahwade901@gmail.com>
2022-06-20 09:05:54 +1200

    shuffle v1
    This version shuffles the alphabet and returns the new alphabet to the main function using a variable called newAlphabet

---
+++ shuffle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace messages
+{
+
+    public class shuffledLetters
+    {
+        private static string newAlphabet;
+
+        public static string shuffle(string newAlphabet)
+        {
+            List<string> alphabet = new List<string>() { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "U", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            List<string> shuffledAlpha = new List<string>();
+
+            var rnd = new Random();
+            var randomized = alphabet.OrderBy(item => rnd.Next());
+
+            foreach (string letter in randomized)
+            {
+                //Console.Write(letter);
+                shuffledAlpha.Add(letter);
+            }
+            Console.WriteLine(string.Join(", ", alphabet));
+            Console.WriteLine(newAlphabet);
+            Console.WriteLine(string.Join(", ", shuffledAlpha));
+
+            return newAlphabet;
+        }
+
+        public static void Main(string[] args)
+        {
+            shuffle(newAlphabet);
+            Console.WriteLine(newAlphabet);
+
+        }
+
+    }
+    
+}
