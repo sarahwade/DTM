8819a99a8a3d58261ba1107f75494e65d4299c92
sarahwade <91816584+sarahwade@users.noreply.github.com>
2022-06-22 12:12:31 +1200

    Update shuffle.cs v1 x2
    Printing the new alphabet list through variable called shuffled in the main routine to be able to use it further. Message prints from level counter.

--- shuffle.cs
+++ shuffle.cs
@@ -1,41 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
 namespace messages
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
-    public class shuffledLetters
+    public struct HackerProfile
     {
-        private static string newAlphabet;
 
-        public static string shuffle(string newAlphabet)
+        public class GoStart
         {
-            List<string> alphabet = new List<string>() { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "U", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
-            List<string> shuffledAlpha = new List<string>();
 
-            var rnd = new Random();
-            var randomized = alphabet.OrderBy(item => rnd.Next());
-
-            foreach (string letter in randomized)
+            public string Shuffle ()
             {
-                //Console.Write(letter);
-                shuffledAlpha.Add(letter);
-            }
-            Console.WriteLine(string.Join(", ", alphabet));
-            Console.WriteLine(newAlphabet);
-            Console.WriteLine(string.Join(", ", shuffledAlpha));
+                    
+                    List<string> alphabet = new List<string>() { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "U", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+                    List<string> shuffledAlpha = new List<string>();
 
-            return newAlphabet;
+                    var rnd = new Random();
+                    var randomized = alphabet.OrderBy(item => rnd.Next());
+
+                    foreach (string letter in randomized)
+                    {
+                        //Console.Write(letter);
+                        shuffledAlpha.Add(letter);
+                    }
+                    Console.WriteLine(string.Join(", ", alphabet));
+                    string newAlphabet = string.Join(", ", shuffledAlpha);
+                    //Console.WriteLine(newAlphabet);
+                    //Console.WriteLine(string.Join(", ", shuffledAlpha));
+
+                    string decrypt = CrackMessage;
+
+                    return newAlphabet;
+            }
+            
+
+            private static string CrackMessage
+            {
+                get
+                {
+                    int userLevel = 0;
+                    List<string> messageList = new List<string>() { "Hello, Welcome to this game",
+                                                                    "Hacking is fun"};
+                    string message = messageList[index: userLevel];
+                    Console.WriteLine(message);
+                    return message;
+                }
+            }
+        
         }
 
         public static void Main(string[] args)
         {
-            shuffle(newAlphabet);
-            Console.WriteLine(newAlphabet);
-
+            // call first class
+            var gs = new GoStart();
+            var shuffled = gs.Shuffle();
+            Console.WriteLine("Hello Test");
+            // shuffled = new Alphabet
+            Console.WriteLine(shuffled);
         }
 
     }
-    
 }
