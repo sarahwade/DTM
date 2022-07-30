5f6047f431cea351395735089044577daf1a7600
sarahwade <91816584+sarahwade@users.noreply.github.com>
2022-07-31 10:59:21 +1200

    messages v2
    this version checks the users answer against the message text box, will need some error handling with capitals and spaces (maybe using split?), also this version prints the alphabets in the debug log. The debug log will also print the wrong or right (next steps will be to congratulate on screen and progress level or reset to retry. Next version aim is to show the encrypted message.

--- message.cs
+++ message.cs
@@ -9,35 +9,23 @@ public class message : MonoBehaviour
 {
   public Text messageText = null;
   public Button messageBut;
-  //char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+  public Button checkBut;
+  int levelCount = 0;
 
+// make new alphabet and print message
   public void shuffleAlpha()
   {
-    string[] alphabet = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r", "s", "t","u","v","w","x","y","z"};
-    string[] mess = {"hello","b","c","d","e","f","g","h"};
-    /*string levelMessage = null;
+    string[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n","o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+    string[] messList = {"hello","b","c","d","e","f","g","h"};
+    string levelMessage = null;
     // change [0] to be level number
-    levelMessage = mess[0];
-    messageText.text = levelMessage;*/
-    messageText.text = mess[0];
+    levelMessage = messList[levelCount];
+    messageText.text = levelMessage;
+    messageText.text = messList[0];
 
-    // hide button after opening the message
+    // hide button after opening the messListage
     messageBut.gameObject.SetActive(false);
 
-/*
-    int keyShift = 2;
-    int keyShiftCount = 0;
-    List<char> newAlpha = new List<char>();
-
-    while (keyShiftCount < keyShift) {
-        // append
-        newAlpha.Add(alphabet[keyShiftCount]);
-        // remove
-        newAlpha.Remove(newAlpha[keyShiftCount]);
-        keyShiftCount++;
-      }
-
-*/
     List<string> newAlpha = new List<string>();
     var rnd = new Random();
     var randomized = alphabet.OrderBy(item => rnd.Next());
@@ -64,4 +52,20 @@ public class message : MonoBehaviour
 
   }
 
-}
+  public Text userInput;
+
+  public void checkAns()
+  {
+    string theirAns = userInput.text;
+    string encAns = messageText.text;
+
+      if (theirAns != encAns)
+      {
+        Debug.Log("Wrong");
+      }
+      else
+      {
+        Debug.Log("Correct");}
+      }
+  }
+
